3a59ae01392bda177f14d94ac9cc93dc18bed940
DoHoangPhi <ih6bmt@gmail.com>
2024-09-07 20:58:26 +0700

    Update code PhieuThu and ThongKe

---
+++ DTO/ChiTietDichVu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class ChiTietDichVu
+    {
+        public string MaPhong { get; set; }
+        public string MaDichVu { get; set; }
+        public float SoLuong { get; set; }
+        public float TongTien { get; set; }
+    }
+}
