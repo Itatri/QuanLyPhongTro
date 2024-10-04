d62fbd712d9728e356a25ce4904c95561089d104
Itatri <trihoangvan16092003@gmail.com>
2024-10-04 20:47:36 +0700

    Up code

---
+++ BLL/DangNhapAdminBLL.cs
@@ -0,0 +1,27 @@
+﻿using DAL;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class DangNhapAdminBLL
+    {
+         private DangNhapAdminDAL dangNhapDAL;
+
+        public DangNhapAdminBLL()
+        {
+            dangNhapDAL = new DangNhapAdminDAL();
+        }
+
+        public bool CapNhatMatKhau(string id, string newPassword)
+        {
+            // Gọi phương thức DAL để cập nhật mật khẩu
+            return dangNhapDAL.UpdatePassword(id, newPassword);
+        }
+
+    }
+}
---
+++ DTO/DangNhapAdminDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class DangNhapAdminDTO
+    {
+        public string ID { get; set; }
+        public string Password { get; set; }
+        public string MaKhuVuc { get; set; }
+        public int TrangThai { get; set; }
+
+        public DangNhapAdminDTO() { }
+
+        public DangNhapAdminDTO(string id, string password, string makhucvuc, int trangThai)
+        {
+            ID = id;
+            Password = password;
+            MaKhuVuc = makhucvuc;
+            TrangThai = trangThai;
+        }
+    }
+}
