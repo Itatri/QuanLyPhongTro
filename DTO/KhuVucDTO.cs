5b55b713990b888cc0ab3811709c45c37cf4cd57
Itatri <trihoangvan16092003@gmail.com>
2024-10-04 20:47:36 +0700

    Up code

---
+++ DTO/KhuVucDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class KhuVucDTO
+    {
+        public string MaKhuVuc { get; set; }
+        public string TenKhuVuc { get; set; }
+        public bool TrangThai { get; set; }
+        // Constructor không tham số
+        public KhuVucDTO()
+        {
+        }
+
+        // Constructor có tham số
+        public KhuVucDTO(string maKhuVuc, string tenKhuVuc, bool trangThai)
+        {
+            MaKhuVuc = maKhuVuc;
+            TenKhuVuc = tenKhuVuc;
+            TrangThai = trangThai;
+        }
+
+
+    }
+}
