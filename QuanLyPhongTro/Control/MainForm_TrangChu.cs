dc2f8cebc5a819bd311933ee796231ae83dba093
Itatri <trihoangvan16092003@gmail.com>
2024-10-12 15:31:01 +0700

    Update giao diện

--- QuanLyPhongTro/Control/MainForm_TrangChu.cs
+++ QuanLyPhongTro/Control/MainForm_TrangChu.cs
@@ -102,5 +102,20 @@ namespace QuanLyPhongTro.Control
         {
 
         }
+
+        private void btnLuuTru_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
